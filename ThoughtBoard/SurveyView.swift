4f5ce3e1de15faadd66921edc4ba94586cfc8362
Agould94 <77901585+Agould94@users.noreply.github.com>
2024-01-03 15:49:00 -0800

    First Commit- Setup, config, & preliminary code.

---
+++ ThoughtBoard/SurveyView.swift
@@ -0,0 +1,36 @@
+//
+//  SurveyView.swift
+//  ThoughtBoard
+//
+//  Created by Alexander Gould on 1/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct SurveyView: View {
+    @State private var answerOne: String = ""
+    @State private var answerTwo: String = ""
+
+    var body: some View {
+        VStack {
+            Text("I am Having A")
+            TextField("Question One", text: $answerOne)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Text("About")
+            TextField("Question Two", text: $answerTwo)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button("Submit") {
+                // Handle the submission of the survey
+            }
+            .padding()
+        }
+        .transition(.slide)
+        .animation(.easeInOut, value: true)
+    }
+}
+
--- ThoughtBoard/ThoughtBoardApp.swift
+++ ThoughtBoard/ThoughtBoardApp.swift
@@ -15,3 +15,4 @@ struct ThoughtBoardApp: App {
         }
     }
 }
+
