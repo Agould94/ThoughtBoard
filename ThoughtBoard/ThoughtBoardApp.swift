f3bb51ace2023e88382b249e79c71e3d47887f26
Agould94 <77901585+Agould94@users.noreply.github.com>
2024-01-02 16:37:17 -0800

    Initial Commit

---
+++ ThoughtBoard/ThoughtBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtBoardApp.swift
+//  ThoughtBoard
+//
+//  Created by Alexander Gould on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
